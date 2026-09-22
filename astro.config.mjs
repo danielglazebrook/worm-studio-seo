4b547e1e2e5d1cc95bb2e0fef7c1026c89a16a43
Daniel Glazebrook <danielg@e-commlab.com>
2026-09-22 09:08:09 +0100

    changed the domain name to include www

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   // Required for the sitemap to generate anything at all - update if the
   // real domain differs.
-  site: "https://wormstudio.co.uk",
+  site: "https://www.wormstudio.co.uk",
   integrations: [tailwind(), sitemap()],
 });
